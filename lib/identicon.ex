1469d2a778b06420dc5481fe8cbdf0ed57b39ce0
Dezande <vincent.corniere@gmail.com>
2021-01-08 09:59:37 +0100

    hash_input method

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,2 +1,13 @@
 defmodule Identicon do
+  @spec main(binary) :: [byte]
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  @spec hash_input(binary) :: [byte]
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+  end
 end
